032c92e4d0b7b981fdfacdc83cc7bbade2d272e9
farrencc <farrencc@tcd.ie>
2024-06-08 18:09:33 +0100

    Complete Restructure in a neon / synthwave theme. Also added the ability to use LaTeX style maths through MathJax.

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Optionally, you can add some interactivity or animations using JavaScript here.
+// For this example, we'll just print a message to the console.
+
+console.log("Welcome to my website");
